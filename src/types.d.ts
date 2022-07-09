f90284ef5fba978e57cda0223ef582e07045d464
NethZ <bynethz@gmail.com>
2022-07-09 14:39:16 -0500

    Resolving issue d.ts, types and enum

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,9 @@
 // type can be created
 // interface is contract of object
 // interface can be extended "extends"
-export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
-export type Visibility = 'great' | 'good' | 'ok' | 'poor'
+
+// export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
+// export type Visibility = 'great' | 'good' | 'ok'
 
 export interface DiaryEntry {
   id: number
@@ -11,3 +12,10 @@ export interface DiaryEntry {
   visibility: Visibility
   comment: string
 }
+
+// is the same lines
+// export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, 'id' | 'date' | 'weather' | 'visibility'>
+
+export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
+
+export type newDiaryEntry = Omit<DiaryEntry, 'id' >
